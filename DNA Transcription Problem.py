c39b0dd54e586407952b42678bcfa51468c20f12
ManVogiatz <mavogiatzakis@gmail.com>
2024-06-04 21:56:02 -0500

    Add files via upload

---
+++ DNA Transcription Problem.py
@@ -0,0 +1,5 @@
+dna_sequence = input("Insert DNA sequence here: ")
+def transcription(dna_sequence):
+    tbdna = dna_sequence.replace("T", "U")
+    return tbdna
+print(transcription(dna_sequence))
